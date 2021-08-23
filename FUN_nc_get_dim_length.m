b59a6c3a03d32086943734fa1b8ca6cc9ccabb18
L.Chi <L.Chi.Ocean@outlook.com>
2021-08-23 00:43:21 -0400

    fix a bug: The netcdf may not be closed after reading data from it in certain cases; This does not lead to errors but keeps the file occuplied by matlab.

--- FUN_nc_get_dim_length.m
+++ FUN_nc_get_dim_length.m
@@ -1,6 +1,7 @@
 function dim_length = FUN_nc_get_dim_length( filename, dim_name_str )
 % find the length of the required dimension
 %
+% V1.01 by L. Chi: add "netcdf.close" at the end of the function
 % V1.00 by L. Chi (L.Chi.Ocean@outlook.com)
 
 ncid = netcdf.open( filename, 'NOWRITE');
@@ -8,3 +9,5 @@ ncid = netcdf.open( filename, 'NOWRITE');
 dimid = netcdf.inqDimID( ncid, dim_name_str );
 
 [~, dim_length] = netcdf.inqDim( ncid, dimid );
+
+netcdf.close(ncid)
