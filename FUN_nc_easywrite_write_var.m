a6988566b211654aca84e7367149dfa0dfa7bed1
L.Chi <L.Chi.Ocean@outlook.com>
2021-10-04 16:46:49 -0400

    minor edits: update comments and styles

--- FUN_nc_easywrite_write_var.m
+++ FUN_nc_easywrite_write_var.m
@@ -3,11 +3,16 @@ function FUN_nc_easywrite_write_var( filename, var_name, data, varargin )
 % FUN_nc_easywrite_write_var( filename, var_name, data, start, count, stride )
 % Write data into an existing variable
 
+% Known problem:
+%     **This does not consider off_set and scale_factor**
+%
+
+% V1.02 by L. Chi: clean useless codes
 % V1.01 by L. Chi: fix a bug: an error occurred when varargin is not empty.
 % V1.00 by L. Chi
 
 
-%% ## 0.
+%% ## 0. check ============================================================
 
 % ####
 if exist(filename,'file')
@@ -21,31 +26,30 @@ is_var_exist = FUN_nc_is_exist_variable( filename, var_name );
 if is_var_exist
     %Pass
 else
-    error(['Variable ' var_name ' does not exist']);
+    error(['Variable ' var_name ' does not exist! Please define it before writing data!']);
 end
 
-%% ## 1. open the netcdf
-% cid = netcdf.create(filename, mode)
-% mode£º
-% 'NC_NOCLOBBER'£º           Prevent overwriting of existing file with the same name.
-% 'NC_SHARE'£º               Allow synchronous file updates.
-% 'NC_64BIT_OFFSET'£º        Allow easier creation of files and variables which are larger than two gigabytes.
+%% ## 1. open the netcdf ==================================================
 
+% open file
 ncid = netcdf.open(filename,'NC_WRITE');
 
+% get variable ID
 varid = netcdf.inqVarID( ncid, var_name );
 
 
-%% ## 2. write into netcdf
+%% ## 2. write into netcdf ================================================
 if nargin == 0
-    netcdf.putVar( ncid, varid, data, varargin{:} );
+    % write the entire variable
+    netcdf.putVar( ncid, varid, data );
 else
+    % write a subset of the variable
     netcdf.putVar( ncid, varid, varargin{:}, data );
 
 end
 
 
-%% ## 3. close file
+%% ## 3. close the file ====================================================
 netcdf.close(ncid);
 
 
