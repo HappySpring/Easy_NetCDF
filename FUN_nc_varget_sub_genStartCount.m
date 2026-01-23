7b3f3a46a2e5fbfce6754e129c2b47b93c437b96
L.Chi <l.chi.ocean@outlook.com>
2026-01-24 00:19:28 +0800

    fix a bugs and improve performance

--- FUN_nc_varget_sub_genStartCount.m
+++ FUN_nc_varget_sub_genStartCount.m
@@ -18,16 +18,16 @@ function [start, count, xloc] = FUN_nc_varget_sub_genStartCount( x, xlimit )
 % V1.42 by L. Chi
 %   Return an error if xlimit(1) > xlimit(2).
 % V1.41 by L. Chi
-%   if x is always within xlimit, ignore the monotonic check. 
-%      This is introduced to read some of HYCOM datasets. (e.g., expt_93.0) 
+%   if x is always within xlimit, ignore the monotonic check.
+%      This is introduced to read some of HYCOM datasets. (e.g., expt_93.0)
 % V1.40 by L. Chi
 %   x & xlimit will be forced as double type
 % V1.30 by L. Chi
-%   Add support for decresing x 
+%   Add support for decresing x
 % V1.20 by L. Chi
-%   Fix a but: In the older version, this function will return 
+%   Fix a but: In the older version, this function will return
 %              start = 0, count = 1, xloc = 1 when x is completely out of
-%              the range defined by xlimit. 
+%              the range defined by xlimit.
 % V1.10 by L. Chi
 %   Add support for -inf, inf in xlimit
 %   Add monotone check.
@@ -36,19 +36,19 @@ function [start, count, xloc] = FUN_nc_varget_sub_genStartCount( x, xlimit )
 
 %%
 
-% decide whether unconstructed grid or not 
-% for unconstructed grid, xlimit may be a list of explicit indexes and it can be incontiguous
+% decide whether unstructured grid or not
+% for unstructured grid, xlimit may be a list of explicit indexes and it can be incontiguous
 if ~isempty(xlimit) && numel(xlimit) > 2
-    % unconstructed grid
-    is_unsconstructed_grid = true;
+    % unstructured grid
+    is_unstructured_grid = true;
 else
-    is_unsconstructed_grid = false;
-end 
+    is_unstructured_grid = false;
+end
 
 % ==========================================================================
-% for unconstructed grids
+% for unstructured grids
 % ==========================================================================
-if is_unsconstructed_grid
+if is_unstructured_grid
     % If xlimit is provided as an explicit list of values (unstructured points),
     % return their locations directly so callers can handle non-contiguous reads.
     % Interpret a multi-element xlimit (>2 elements) as either a list of indices
@@ -74,9 +74,9 @@ if is_unsconstructed_grid
     end
 
 else
-% ==========================================================================
-% for normal cnodition (constructed grids)
-% ==========================================================================
+    % ==========================================================================
+    % for normal cnodition (constructed grids)
+    % ==========================================================================
 
 
     % preparation: force x as double type
@@ -89,7 +89,7 @@ else
     end
 
     if xlimit(1) > xlimit(2)
-    error('xlimit(2) >= xlimit(1) is required for all dimensions!') 
+        error('xlimit(2) >= xlimit(1) is required for all dimensions!')
     end
 
     %
@@ -99,58 +99,58 @@ else
         is_x_decrese = 1;
         x = x(end:-1:1);
     else
-    % the input x is not monotonic
-    if all( x >= xlimit(1) & x <= xlimit(2)  )
-        start = 0; % matrix begin with 0 in nc files.
-        count = length(x);
-        xloc = [1:length(x)];
-        
-        warning('x is not monotonic, however, data will still be loaded since the entire domain is within the required limit! Please be careful about the output' )
-        return
-    else
-        
-        ck_tem_ind = find( x >= xlimit(1) & x <= xlimit(2) );
-        
-        if all( diff(ck_tem_ind) == 1 ) 
-            
-            % Skip the error if the data is monotonic within the required limit.
-            % This is introduced from reading HYCOM data (exp 93.0)
-            
-            %if all( diff( x(ck_tem_ind) ) > 0 )
-            %     is_x_decrese = 0; 
-            %elseif all( diff( x(ck_tem_ind) ) < 0 )
-            %     is_x_decrese = 1;
-            %     x = x(end:-1:1);
-            %else
-            %    error('Unexpected value!');
-            %end
-            
-            if median( diff( x(ck_tem_ind) ) > 0 )
+        % the input x is not monotonic
+        if all( x >= xlimit(1) & x <= xlimit(2)  )
+            start = 0; % matrix begin with 0 in nc files.
+            count = length(x);
+            xloc = [1:length(x)];
+
+            warning('x is not monotonic, however, data will still be loaded since the entire domain is within the required limit! Please be careful about the output' )
+            return
+        else
+
+            ck_tem_ind = find( x >= xlimit(1) & x <= xlimit(2) );
+
+            if all( diff(ck_tem_ind) == 1 )
+
+                % Skip the error if the data is monotonic within the required limit.
+                % This is introduced from reading HYCOM data (exp 93.0)
+
+                %if all( diff( x(ck_tem_ind) ) > 0 )
+                %     is_x_decrese = 0;
+                %elseif all( diff( x(ck_tem_ind) ) < 0 )
+                %     is_x_decrese = 1;
+                %     x = x(end:-1:1);
+                %else
+                %    error('Unexpected value!');
+                %end
+
+                if median( diff( x(ck_tem_ind) ) > 0 )
                     is_x_decrese = 0;
-            
-            elseif median( diff( x(ck_tem_ind) ) < 0 )
+
+                elseif median( diff( x(ck_tem_ind) ) < 0 )
                     is_x_decrese = 1;
                     x = x(end:-1:1);
+                else
+                    error('Unexpected value!');
+                end
+
+                warning(['x is not monotonic, however, it is monotonic within the inquired limit! Plase check its raw values!']);
+
             else
-                error('Unexpected value!');
+                % x must be monotonic within the required limit.
+                error('x must be monotonic.');
             end
-            
-            warning(['x is not monotonic, however, it is monotonic within the inquired limit! Plase check its raw values!']);
-            
-        else          
-            % x must be monotonic within the required limit.
-            error('x must be monotonic.');
         end
-    end
     end
 
     % Get location of start
     if isinf(xlimit(1))
         start_loc = 1;
     else
-        tem = ( x - xlimit(1) ); 
-        tem( tem<0 ) = nan; 
-        
+        tem = ( x - xlimit(1) );
+        tem( tem<0 ) = nan;
+
         if all( isnan(tem) )
             % All x is smaller than xlimit(1)
             start_loc = nan;
@@ -158,35 +158,35 @@ else
             % at least one x >= xlimit(1)
             [~, start_loc] = min(tem);
         end
-            clear tem
+        clear tem
     end
 
     % Get location of end
     if isinf(xlimit(2))
         end_loc = length(x);
     else
-        tem2 = ( x - xlimit(2) ); 
-        tem2( tem2>0 ) = nan; 
-        
+        tem2 = ( x - xlimit(2) );
+        tem2( tem2>0 ) = nan;
+
         if all(isnan( tem2 ))
             % all x is larger than xlimit(2)
             end_loc = nan;
         else
-            % At least one x <= xlimit(2) 
+            % At least one x <= xlimit(2)
             [~, end_loc] = max(tem2);
         end
-            clear tem2
+        clear tem2
     end
 
-    % 
+    %
     if isnan( start_loc ) || isnan( end_loc )
-        % x is completely out of the range defined by xlimit 
+        % x is completely out of the range defined by xlimit
         start = nan;
         count = 0;
         xloc  = [];
-    else 
+    else
         % At least one x is located in [ xlimit(1),  xlimit(2)]
-        start = start_loc - 1; % matrix begin with 0 in nc files. 
+        start = start_loc - 1; % matrix begin with 0 in nc files.
         count = end_loc - start_loc + 1;
         xloc  = start_loc:end_loc;
     end
